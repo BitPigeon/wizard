80e1119f92fb23d9471163243537bed200cbacbf
TheRealPenguin12 <desmond.vasicek@protonmail.com>
2023-04-21 07:36:32 -0700

    Misc.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from tkinter import Tk, END
 from tkinter.scrolledtext import ScrolledText
-from tkinter.messagebox import askokcancel
+from tkinter.messagebox import askokcancel, showwarning
 import re
 import webbrowser
 import os
@@ -24,11 +24,13 @@ class App(Tk):
         self.inside_style_tag = False
         self.editor = ScrolledText(self, undo=True)
         self.editor.pack(fill="both", expand=True)
+        self.editor.config(tabs=16)
         self.editor.config(font=("Liberation Mono", 9))
         self.editor.tag_configure("tag", foreground="DarkBlue", font=self.bold_font)
-        self.editor.tag_configure("doctype", foreground="Blue2", font=self.bold_font)
+        self.editor.tag_configure("doctype", foreground="Blue3", font=self.bold_font)
         self.editor.tag_configure("comment", foreground="Gray64", font=self.font)
-        self.editor.tag_configure("sel", background="Gray98")
+        self.editor.tag_configure("sel", background="Gray88")
+        self.editor.bind("<KeyPress>", self.keydown)
         self.editor.bind("<<Modified>>", self.highlight)
         self.editor.bind("<F5>", self.run)
         self.editor.bind("<Control-s>", self.save)
@@ -70,8 +72,8 @@ class App(Tk):
                     elif re.match("</style.*>", match.group(0)):
                         self.inside_style_tag = False
                         self.editor.tag_add("tag", start, end)
-                    index[1] += len(match.group(0))
-                    text = text[len(match.group(0)):]
+                    index[1] += 1
+                    text = text[1:]
                 elif text[0] == "\n":
                     index[0] += 1
                     index[1] = 0
@@ -85,7 +87,26 @@ class App(Tk):
 
     def run(self, _):
         "Ask to save and run the file."
-        if askokcancel("Run?", "To run this, you must first save the file."):
+        if not self.read_only:
+            if askokcancel("Run?", "To run this, you must first save the file."):
+                logging.info("File saved.")
+                folder = os.environ['HOME']
+                try:
+                    file = open(f"{folder}/{self.file_name}", "w+")
+                except (OSError, IOError):
+                    file = open(f"{folder}/{self.file_name}", "x+")
+                file.write(self.editor.get(1.0, END))
+                file.close()
+                logging.info("File run.")
+                webbrowser.open(f'file:///{folder}/{self.file_name}')
+            else:
+                logging.warning("File run canceled.")
+        else:
+            self.show_ro_error()
+
+    def save(self, _):
+        "Save the file."
+        if not self.read_only:
             logging.info("File saved.")
             folder = os.environ['HOME']
             try:
@@ -94,27 +115,31 @@ class App(Tk):
                 file = open(f"{folder}/{self.file_name}", "x+")
             file.write(self.editor.get(1.0, END))
             file.close()
-            logging.info("File run.")
-            webbrowser.open(f'file:///{folder}/{self.file_name}')
         else:
-            logging.warning("File run canceled.")
+            self.show_ro_error()
 
-    def save(self, _):
-        "Save the file."
-        logging.info("File saved.")
-        folder = os.environ['HOME']
+    def open_from_external_source(self, site):
+        "Open a file from a website."
         try:
-            file = open(f"{folder}/{self.file_name}", "w+")
-        except (OSError, IOError):
-            file = open(f"{folder}/{self.file_name}", "x+")
-        file.write(self.editor.get(1.0, END))
-        file.close()
+            root.editor.insert(1.0, requests.get(site).text)
+            logging.info(f"File loaded from {site}.")
+        except requests.exceptions.ConnectionError:
+            logging.warning("File loading failed.")
+        self.editor.config(state="disabled")
+        self.read_only = True
+
+    def keydown(self, _):
+        "If it is read only, no keys can be typed."
+        if self.read_only == True:
+            self.show_ro_error()
+
+    def show_ro_error(self):
+        "Show warning that you cannot edit the document."
+        title = "Read Only Document"
+        message = "You can not edit this document as it is not yours."
+        showwarning(title, message)
 
 root = App()
-try:
-    root.editor.insert(1.0, requests.get("https://example.com").text)
-    logging.info("File loaded from external source.")
-except requests.exceptions.ConnectionError:
-    logging.warning("File loading failed.")
+root.open_from_external_source("https://docs.python.org/3/library/types/")
 root.editor.focus_set()
 root.mainloop()
